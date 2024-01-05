e42b2972775cac2e53072e9b04cb4e987f2cc0d1
cristian camacho <cristianduvang12@hotmail.com>
2024-01-04 21:11:06 -0500

    puerto

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const express = require('express');
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3001;
 const cors = require('cors');
 const emailRoutes = require('./src/routes/emailRoutes');
 
